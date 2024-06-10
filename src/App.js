9c67457c706fd7f04e09051bec2bdb49678c667a
Ben Langlois <benlangloiss@gmail.com>
2024-06-10 19:57:50 -0400

    progress on login, seperating components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.scss';
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+import $, { event } from 'jquery';
+import Header from './header.js';
 import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
 
-
-const Header = () => {
-  return(
-    <header id='header' className='flex justify-center items-center h-20 gap-x-6'> {/*border-slate-400 border-b on scroll down */}
-      <h1 className='text-3xl font-display font-medium'>Shelfd</h1>
-      <input type="text" name="search" className='w-3/6 rounded outline outline-slate-400 p-1 text-center' placeholder='Search Books, Authors, Genres etc'/>
-      <PersonOutlineIcon className='hover:scale-110 h-8 transform transition duration-50 cursor-pointer'/>
-      <PeopleOutlineIcon className='hover:scale-110 h-8 transform transition duration-50 cursor-not-allowed'/> {/* will change cursor once functional */}
-    </header>
-  )
+const hideCTA = (e) => {
+console.log(e)
 }
 
 const Landing = () => {
@@ -46,14 +39,24 @@ const Landing = () => {
         <div className='outline outline-red-400 rounded'>&nbsp;</div>  {/* Will replace will API or something, just divs to figure out css*/}
       
       </div>
-      <div className='float-right w-2/4 content-center text-left p-6 m-6 border-400 border-l'> {/* login/register buttons (that lead to form) */}
-        <h1 className='text-5xl font-semibold font-display'>Discover, Discuss, and Track your next reads along tens of other users</h1>
-        <div id='form-selector' className='flex flex-row gap-4 pt-6'>
-          <Button size='large'>Login</Button>          
-          <Button size='large' variant="outlined">Sign Up</Button>        
+      <div id='action-container' className='w-2/4 content-center text-left p-6 m-6 border-400 border-l'> {/* login/register buttons (that lead to form) */}
+        <div id='calltoaction' className=''>
+          <h1 className='text-5xl font-semibold font-display'>Discover, Discuss, and Track your next reads along tens of other users</h1>
+          <div id='form-selector' className='flex flex-row gap-4 pt-6'>
+            <Button size='large' onClick={() => hideCTA(event.target)}>Login</Button>          
+            <Button size='large' variant="outlined" onClick={{}}>Sign Up</Button>        
+          </div>
         </div>
+        <div id='login' className='hidden'> {/* login form to display */}
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
+          <TextField id="outlined-basic" label="Password" variant="outlined" />
+          <Button size='large' variant="outlined" onClick={{}}>Login</Button>        
+        </div>
+        <div id='signup' className='hidden'> {/* signup form to display */}
 
+        </div>      
       </div>
+
     </div>
   )
 } 
