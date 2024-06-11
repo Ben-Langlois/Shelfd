98c795e87c7fb69d9fbd47ee8f0d5994797a4e04
Ben Langlois <benlangloiss@gmail.com>
2024-06-10 21:57:42 -0400

    lots of progress on login

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import Header from './header.js';
 import Button from '@mui/material/Button';
 import { TextField } from '@mui/material';
 
-const hideCTA = (e) => {
-console.log(e)
+const toggleForm = (form) => {
+  $('#login').toggleClass('hidden');
+  $('#signup').toggleClass('hidden');
 }
 
 const Landing = () => {
   return(
     <div id='landing' className='flex flex-auto p-6 '>
-      <div className='grid grid-cols-5 grid-rows-5 gap-6 w-2/4 float-left'> {/* grid of books, maybe 'featured' books? */}
+      <div className='grid grid-cols-6 grid-rows-5 gap-6 w-2/4 float-left'> {/* grid of books, maybe 'featured' books? */}
+        <div className='outline outline-red-400 rounded'>&nbsp;</div>  {/* Will replace will API or something, just divs to figure out css*/}
+        <div className='outline outline-red-400 rounded'>&nbsp;</div>  {/* Will replace will API or something, just divs to figure out css*/}
+        <div className='outline outline-red-400 rounded'>&nbsp;</div>  {/* Will replace will API or something, just divs to figure out css*/}
+        <div className='outline outline-red-400 rounded'>&nbsp;</div>  {/* Will replace will API or something, just divs to figure out css*/}
+        <div className='outline outline-red-400 rounded'>&nbsp;</div>  {/* Will replace will API or something, just divs to figure out css*/}
         <div className='outline outline-red-400 rounded'>&nbsp;</div>  {/* Will replace will API or something, just divs to figure out css*/}
         <div className='outline outline-red-400 rounded'>&nbsp;</div>  {/* Will replace will API or something, just divs to figure out css*/}
         <div className='outline outline-red-400 rounded'>&nbsp;</div>  {/* Will replace will API or something, just divs to figure out css*/}
@@ -39,24 +45,28 @@ const Landing = () => {
         <div className='outline outline-red-400 rounded'>&nbsp;</div>  {/* Will replace will API or something, just divs to figure out css*/}
       
       </div>
-      <div id='action-container' className='w-2/4 content-center text-left p-6 m-6 border-400 border-l'> {/* login/register buttons (that lead to form) */}
-        <div id='calltoaction' className=''>
+      <div id='action-container' className='w-2/4 content-center text-left p-6 m-6 border-400 border-l transition-opacity duration-1000'> {/* login/register buttons (that lead to form) */}
+        <div id='calltoaction' className='opacity-100 mb-6'>
           <h1 className='text-5xl font-semibold font-display'>Discover, Discuss, and Track your next reads along tens of other users</h1>
-          <div id='form-selector' className='flex flex-row gap-4 pt-6'>
-            <Button size='large' onClick={() => hideCTA(event.target)}>Login</Button>          
-            <Button size='large' variant="outlined" onClick={{}}>Sign Up</Button>        
+        </div>
+        <div id='login' className='flex flex-col flex-wrap gap-6'> {/* login form to display */}
+          <TextField id="outlined-basic" required label="Email" variant="outlined" />
+          <TextField id="outlined-basic" required label="Password" variant="outlined" />
+          <div id='buttons' className='flex gap-6 items-center'>
+            <Button size='large' className='h-1/4 w-1/4' variant="outlined" onClick={{}}>Login</Button> 
+            <Button className='h-1/4' variant="text" onClick={() => toggleForm()}>Dont have an account?</Button>            
           </div>
         </div>
-        <div id='login' className='hidden'> {/* login form to display */}
-          <TextField id="outlined-basic" label="Email" variant="outlined" />
-          <TextField id="outlined-basic" label="Password" variant="outlined" />
-          <Button size='large' variant="outlined" onClick={{}}>Login</Button>        
-        </div>
-        <div id='signup' className='hidden'> {/* signup form to display */}
-
+        <div id='signup' className='hidden flex flex-col flex-wrap gap-6'> {/* signup form to display */}
+          <TextField id="outlined-basic" label="First Name" variant="outlined" />
+          <TextField id="outlined-basic" required label="Email" variant="outlined" />
+          <TextField id="outlined-basic" required label="Password" variant="outlined" />
+          <div id='buttons' className='flex gap-6 items-center'>
+            <Button size='large' className='h-1/4 w-1/4' variant="outlined" onClick={{}}>Sign Up</Button> 
+            <Button className='h-1/4' variant="text" onClick={() => toggleForm()}>Back</Button>            
+          </div>
         </div>      
       </div>
-
     </div>
   )
 } 
