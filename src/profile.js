aefa78d32230f9b004fbebc0276beda4cbe03e54
Ben_Langlois <benlangloiss@gmail.com>
2024-08-31 18:34:06 -0400

    working on profile comp functionality

--- src/profile.js
+++ src/profile.js
@@ -23,29 +23,16 @@ export const Profile = () => {
         </div>
       </div> 
       <div id='display-shelves' className='flex flex-col text-left'>
-      { // this would ref the state object that <BookShelves /> is getting but NOT as <Shelf />s 
-          // but for now just an array of titles (in local state)
-        // what if bookshelf is empty? do i still want to show it?
-          // I think thats a 'solve once I do backend' problem, cause the object just doesnt exist rn
-        shelves.map((e, i) => {
-          return (
-            <div id='displayShelf' className='flex flex-col gap-y-4 mb-6'>
-              <h1 className='text-xl border-b'>{e.title}</h1>
-              <div id='books' className='flex gap-6'>
-              {       
-                e.books.map((book) => {
-                  return(
-                    <div id='displayCover' className='h-24 w-16 border border-red'>
-                      {book.title}
-                    </div>
-                  )
-                })
-              }                  
-              </div>
-            </div>
-          )
-        })
-      }
+        { // this would ref the state object that <BookShelves /> is getting but NOT as <Shelf />s 
+            // but for now just an array of titles (in local state)
+          // what if bookshelf is empty? do i still want to show it?
+            // I think thats a 'solve once I do backend' problem, cause the object just doesnt exist rn
+          shelves.map((e, i) => {
+            return (
+              <DisplayShelf title={e.title} books={e.books} />
+            )
+          })
+        }
       </div>
       <div id='social' className='flex flex-col absolute w-1/3 top-0 right-[-35%] justify-start text-start'>
         <h1 className=' text-xl'>Following</h1>
@@ -57,6 +44,28 @@ export const Profile = () => {
           <Avatar className=''>U</Avatar>
           <Avatar className=''>U</Avatar>
         </div>
+      </div>
+    </div>
+  )
+}
+
+
+const DisplayShelf = ( {title, books} ) => {
+
+
+  return (
+    <div id='displayShelf' className='flex flex-col gap-y-4 mb-6'>
+      <h1 className='text-xl border-b'>{title}</h1>
+      <div id='books' className='flex gap-6'>
+        {       
+          books.map((book) => {
+            return(
+              <div id='displayCover' className='h-24 w-16 border border-red'>
+                {book.title}
+              </div>
+            )
+          })
+        }                  
       </div>
     </div>
   )
